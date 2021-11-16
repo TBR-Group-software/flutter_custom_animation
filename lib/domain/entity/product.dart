3532a15046af3a7e4aa42cdfcfdb76a24509b88a
VadimBliz <vadimbliz98@gamil.com>
2021-11-16 14:53:46 +0200

    add app entities

---
+++ lib/domain/entity/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id, name;
+  final int volume;
+  final double price;
+  final List<String> images;
+
+  Product(
+      {required this.id,
+      required this.name,
+      required this.volume,
+      required this.price,
+      required this.images});
+}
