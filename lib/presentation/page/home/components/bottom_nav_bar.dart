631f3ca8b18b327509adf8727149161610de281a
VadimBliz <vadimbliz98@gamil.com>
2021-11-18 09:12:47 +0200

    change bottom nav bar

--- lib/presentation/page/home/components/bottom_nav_bar.dart
+++ lib/presentation/page/home/components/bottom_nav_bar.dart
@@ -3,7 +3,7 @@ import 'package:flutter_custom_animation/presentation/theme/icons.dart';
 import 'package:flutter_custom_animation/presentation/theme/palette.dart';
 import 'package:flutter_custom_animation/presentation/theme/text_styles.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class AppBottomNavBar extends StatefulWidget {
   const AppBottomNavBar({Key? key}) : super(key: key);
@@ -15,27 +15,35 @@ class AppBottomNavBar extends StatefulWidget {
 class _AppBottomNavBarState extends State<AppBottomNavBar> {
   int _selectedIndex = 0;
 
-  PersistentBottomNavBarItem _buildItem({
+  double _calculateBottomPadding() {
+    final double bottomPadding = MediaQuery.of(context).padding.bottom;
+    if (bottomPadding < 10) {
+      return 10;
+    } else {
+      return bottomPadding;
+    }
+  }
+
+  SalomonBottomBarItem _buildItem({
     required String text,
     required String icon,
     required int index,
   }) =>
-      PersistentBottomNavBarItem(
+      SalomonBottomBarItem(
         icon: SvgPicture.asset(
           icon,
           color: index == _selectedIndex ? Colors.white : AppPalette.gray,
         ),
-        title: text,
-        iconSize: 24,
-        textStyle: AppTextStyles.playfairDisplayRegular15White,
-        activeColorPrimary: AppPalette.liteRed,
-        activeColorSecondary: Colors.white,
+        title: Text(
+          text,
+          style: AppTextStyles.playfairDisplayRegular15White,
+        ),
+        selectedColor: AppPalette.liteRed,
       );
 
-  List<PersistentBottomNavBarItem> _buildItems(
+  List<SalomonBottomBarItem> _buildItems(
       {required List<String> names, required List<String> icons}) {
-    final List<PersistentBottomNavBarItem> result =
-        <PersistentBottomNavBarItem>[];
+    final List<SalomonBottomBarItem> result = <SalomonBottomBarItem>[];
 
     for (int i = 0; i < names.length; i++) {
       result.add(_buildItem(text: names[i], icon: icons[i], index: i));
@@ -46,35 +54,38 @@ class _AppBottomNavBarState extends State<AppBottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return PersistentBottomNavBar(
-      margin: EdgeInsets.zero,
-      navBarStyle: NavBarStyle.style7,
-      navBarDecoration: NavBarDecoration(
-        borderRadius: BorderRadius.circular(10),
+    return Container(
+      padding: EdgeInsets.only(
+          bottom: _calculateBottomPadding(), left: 25, right: 25, top: 20),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30), topRight: Radius.circular(30)),
       ),
-      navBarEssentials: NavBarEssentials(
-          navBarHeight: 50,
-          items: _buildItems(
-            names: <String>[
-              'Home',
-              'Cart',
-              'Navigation',
-              'People',
-            ],
-            icons: <String>[
-              AppIcons.home,
-              AppIcons.shoppingCart,
-              AppIcons.navigation,
-              AppIcons.people,
-            ],
-          ),
-          backgroundColor: Colors.white,
-          selectedIndex: _selectedIndex,
-          onItemSelected: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          }),
+      child: SalomonBottomBar(
+        selectedColorOpacity: 1,
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        margin: EdgeInsets.zero,
+        items: _buildItems(
+          names: <String>[
+            'Home',
+            'Cart',
+            'Navigation',
+            'People',
+          ],
+          icons: <String>[
+            AppIcons.home,
+            AppIcons.shoppingCart,
+            AppIcons.navigation,
+            AppIcons.people,
+          ],
+        ),
+      ),
     );
   }
 }
