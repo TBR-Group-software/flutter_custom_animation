a3c6cc9d835d39242bde7e4e010b6798c8791371
VadimBliz <vadimbliz98@gamil.com>
2021-11-24 15:19:31 +0200

    add new text styles

--- lib/presentation/theme/text_styles.dart
+++ lib/presentation/theme/text_styles.dart
@@ -64,4 +64,52 @@ class AppTextStyles {
       letterSpacing: -0.30000001192092896,
       fontWeight: FontWeight.normal,
       height: 1);
+
+  static const TextStyle playfairDisplayRegular24Black = TextStyle(
+      color: AppPalette.black,
+      fontFamily: 'PlayfairDisplay',
+      fontSize: 24,
+      letterSpacing: -0.30000001192092896,
+      fontWeight: FontWeight.normal,
+      height: 1);
+
+  static const TextStyle playfairDisplayRegular24White = TextStyle(
+      color: Colors.white,
+      fontFamily: 'PlayfairDisplay',
+      fontSize: 24,
+      letterSpacing: -0.30000001192092896,
+      fontWeight: FontWeight.normal,
+      height: 1);
+
+  static const TextStyle display26RegularWhite = TextStyle(
+      color: Colors.white,
+      fontFamily: 'SFProDisplay',
+      fontSize: 26,
+      letterSpacing: -0.30000001192092896,
+      fontWeight: FontWeight.normal,
+      height: 1);
+
+  static const TextStyle display15RegularWhite = TextStyle(
+      color: Colors.white,
+      fontFamily: 'SFProDisplay',
+      fontSize: 15,
+      letterSpacing: -0.30000001192092896,
+      fontWeight: FontWeight.normal,
+      height: 1);
+
+  static const TextStyle display16RegularWhite = TextStyle(
+      color: Colors.white,
+      fontFamily: 'SFProDisplay',
+      fontSize: 16,
+      letterSpacing: -0.30000001192092896,
+      fontWeight: FontWeight.normal,
+      height: 1);
+
+  static const TextStyle display10RegularWhite = TextStyle(
+      color: Color.fromRGBO(255, 245, 245, 1),
+      fontFamily: 'SFProDisplay',
+      fontSize: 10,
+      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+      fontWeight: FontWeight.normal,
+      height: 1);
 }
