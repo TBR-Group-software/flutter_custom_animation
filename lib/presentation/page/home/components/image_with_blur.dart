c7e8dfa78db2a1e3d9474c076e145e24b686b738
VadimBliz <vadimbliz98@gamil.com>
2021-11-24 15:21:25 +0200

    fix blur

--- lib/presentation/page/home/components/image_with_blur.dart
+++ lib/presentation/page/home/components/image_with_blur.dart
@@ -1,38 +1,25 @@
 import 'dart:math' as math;
-import 'dart:ui';
 
+import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 
 class ImageWithBlur extends StatelessWidget {
   final String image;
+  final bool blurred;
 
-  const ImageWithBlur({required this.image, Key? key}) : super(key: key);
+  const ImageWithBlur({required this.image, this.blurred = true, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Transform.rotate(
       angle: -math.pi / -18,
       alignment: Alignment.bottomCenter,
-      child: Container(
-        width: 69.22,
-        height: 164,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(image),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: ClipRRect(
-          child: Padding(
-            padding: const EdgeInsets.all(5),
-            child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-              child: Container(
-                color: Colors.white.withOpacity(0.0),
-              ),
-            ),
-          ),
-        ),
+      child: Center(
+        child: Image.asset(
+          image,
+          fit: BoxFit.scaleDown,
+        ).blurred(blur: blurred ? 2 : 0, colorOpacity: 0),
       ),
     );
   }
