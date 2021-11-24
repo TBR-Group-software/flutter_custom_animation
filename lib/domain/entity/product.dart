d9f3f969cea3fc5468b81e45e8a28b99ce7a276e
VadimBliz <vadimbliz98@gamil.com>
2021-11-24 15:20:49 +0200

    add description to product entity

--- lib/domain/entity/product.dart
+++ lib/domain/entity/product.dart
@@ -1,13 +1,15 @@
 class Product {
-  final String id, name;
+  final String id, name, description;
   final int volume;
   final double price;
   final List<String> images;
 
-  Product(
-      {required this.id,
-      required this.name,
-      required this.volume,
-      required this.price,
-      required this.images});
+  Product({
+    required this.id,
+    required this.name,
+    required this.volume,
+    required this.price,
+    required this.images,
+    required this.description,
+  });
 }
