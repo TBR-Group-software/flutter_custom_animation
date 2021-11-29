7d0c638ddaca4f5bae670c9d09e0dc508605ffd8
VadimBliz <vadimbliz98@gamil.com>
2021-11-29 09:26:11 +0200

    update dependency injection

--- lib/backbone/dependency_injection.dart
+++ lib/backbone/dependency_injection.dart
@@ -1,5 +1,27 @@
+import 'package:flutter_custom_animation/data/service/mock/cart.dart';
+import 'package:flutter_custom_animation/data/service/mock/categories.dart';
+import 'package:flutter_custom_animation/data/service/mock/subcategory.dart';
+import 'package:flutter_custom_animation/domain/service/cart.dart';
+import 'package:flutter_custom_animation/domain/service/catagories.dart';
+import 'package:flutter_custom_animation/domain/service/subcategory.dart';
+import 'package:flutter_custom_animation/domain/usecase/cart/get.dart';
+import 'package:flutter_custom_animation/domain/usecase/category/get_all.dart';
+import 'package:flutter_custom_animation/domain/usecase/subcategory/get_all.dart';
+import 'package:flutter_custom_animation/presentation/bloc/home_page/bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt sl = GetIt.instance;
 
-void init() {}
+void init() {
+  sl.registerLazySingleton<SubcategoryService>(() => MockSubcategoryService());
+  sl.registerLazySingleton<CategoriesService>(() => MockCategoriesService());
+  sl.registerLazySingleton<CartService>(() => MockCartService());
+  sl.registerLazySingleton<GetCartUseCase>(
+      () => ServiceGetCartUseCase(sl.get()));
+  sl.registerLazySingleton<GetAllCategoriesUseCase>(
+      () => ServiceGetAllCategoriesUseCase(sl.get()));
+  sl.registerLazySingleton<GetAllSubcategoriesUseCase>(
+      () => ServiceGetAllSubcategoriesUseCase(sl.get()));
+  sl.registerLazySingleton<HomePageBloc>(
+      () => HomePageBloc(sl.get(), sl.get()));
+}
