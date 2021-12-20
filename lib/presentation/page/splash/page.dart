8e394226bcb1fdabb6e529f610093acbdcfc67b2
VadimBliz <vadimbliz98@gmail.com>
2021-12-20 09:12:54 +0200

    update flutter version

--- lib/presentation/page/splash/page.dart
+++ lib/presentation/page/splash/page.dart
@@ -1,9 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_animation/backbone/dependency_injection.dart'
+    as di;
+import 'package:flutter_custom_animation/presentation/bloc/home_page/bloc.dart';
 import 'package:flutter_custom_animation/presentation/page/splash/components/logo_animation.dart';
 import 'package:flutter_custom_animation/presentation/theme/palette.dart';
-import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    final HomePageBloc bloc = di.sl.get();
+    bloc.add(const HomePageEvent.getInitialData());
+  }
 
   @override
   Widget build(BuildContext context) {
